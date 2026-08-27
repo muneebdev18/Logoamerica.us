d59eda3e2a25674bc69542d6111eb48c30a265ac
Muhammad Muneeb <muneebdev18@gmail.com>
2026-08-27 05:24:09 +0500

    Update Code

--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1ck6hg9._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/src_0iarxmu._.js","static/chunks/node_modules_next_0ig31tq._.js"],
-    source: "entry"
-});
